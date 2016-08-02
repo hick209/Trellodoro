3728d6cfa094a0616ac109c08d57ce93ca97cc50
Nivaldo Bondanca <hick209@gmail.com>
2016-08-02 18:44:59 -0300

    Added initial data to Tasks screen

--- app/src/main/java/info/nivaldobondanca/trellodoro/MockData.java
+++ app/src/main/java/info/nivaldobondanca/trellodoro/MockData.java
@@ -12,28 +12,39 @@ import info.nivaldobondanca.trellodoro.model.TrelloList;
 import info.nivaldobondanca.trellodoro.model.TrellodoroCard;
 import info.nivaldobondanca.trellodoro.model.TrellodoroList;
 import info.nivaldobondanca.trellodoro.model.factory.BoardFactory;
+import info.nivaldobondanca.trellodoro.model.factory.CardFactory;
 import info.nivaldobondanca.trellodoro.model.factory.ListFactory;
 
 /**
  * @author Nivaldo Bondança
  */
 public class MockData {
+
+	private static TrelloBoard helloBoard = BoardFactory.create("209L", "Hello World!",
+			Arrays.asList(
+					ListFactory.create("123to7do", "Scratch", Arrays.<TrelloCard>asList(
+							CardFactory.create("card-2", "Do me!", 0, 0),
+							CardFactory.create("card-5", "TODO, XXX, FIXME", 0, 0)
+					)),
+					ListFactory.create("123doing", "Writing", Arrays.<TrelloCard>asList(
+							CardFactory.create("card-3", "Almost there!", 42 * 60 * 1000, 1),
+							CardFactory.create("card-4", "Oh yeah ;)", 0, 0)
+					)),
+					ListFactory.create("123done1", "Published", Arrays.<TrelloCard>asList(
+							CardFactory.create("card-1", "Hello World card!", 5 * 60_000, 1),
+							CardFactory.create("card-6", "This is done!", 0, 0)
+					))
+			));
+	private static TrelloBoard emptyBoard = BoardFactory.create("1001L", "Empty board", Collections.<TrelloList>emptyList());
+
 	public static List<TrelloBoard> trelloBoards() {
-		return Arrays.asList(
-				BoardFactory.create("209L", "Hello World!",
-						Arrays.asList(
-								ListFactory.create("123to7do", "Scratch", Collections.<TrelloCard>emptyList()),
-								ListFactory.create("123doing", "Writing", Collections.<TrelloCard>emptyList()),
-								ListFactory.create("123done1", "Published", Collections.<TrelloCard>emptyList())
-						)),
-				BoardFactory.create("1001L", "Empty board", Collections.<TrelloList>emptyList())
-		);
+		return Arrays.asList(helloBoard, emptyBoard);
 	}
 
 
-	private static TrellodoroList todoList;
-	private static TrellodoroList doingList;
-	private static TrellodoroList doneList;
+	private static TrellodoroList todoList = ListFactory.create(helloBoard.lists().get(0), TrellodoroList.TYPE_TODO);
+	private static TrellodoroList doingList = ListFactory.create(helloBoard.lists().get(1), TrellodoroList.TYPE_DOING);
+	private static TrellodoroList doneList = ListFactory.create(helloBoard.lists().get(2), TrellodoroList.TYPE_DONE);
 
 	public static void saveLists(TrellodoroList todoList, TrellodoroList doingList, TrellodoroList doneList) {
 		MockData.todoList = todoList;
